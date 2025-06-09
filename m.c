06cbca0343a7c103b2bea0d32702eb6871c4b595
tr <digital74839@gmail.com>
2025-06-09 21:06:14 +0530

    Update m.c

--- m.c
+++ m.c
@@ -10,8 +10,8 @@
 #include <arpa/inet.h>
 
 #define MAX_THREADS 2000
-#define MIN_PACKET_SIZE 60
-#define MAX_PACKET_SIZE 120
+#define MIN_PACKET_SIZE 900
+#define MAX_PACKET_SIZE 1024
 #define EXPIRATION_YEAR 2028
 #define EXPIRATION_MONTH 4
 #define EXPIRATION_DAY 15
